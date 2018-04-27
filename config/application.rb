a74fbea664ac223d1663f14aeaa60908c8a4176a
myrabel <memusi2005@gmail.com>
2018-04-27 13:00:47 +0200

    timezone

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module ObstacleDetection
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = "Harare"
+    heroku config:add TZ="Harare"
   end
 end
